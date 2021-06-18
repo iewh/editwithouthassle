7a5769bb8d0c293d883e36b0393aaa581b8bbcbd
deepanshusharmaofficial <deepanshusharmaofficial@gmail.com>
2021-06-18 19:52:28 +0530

    contact form connected with spreadsheet

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,22 +1,22 @@
-var form = document.getElementById("contact-form");
+var form = document.getElementById("contactForm");
 
 async function handleSubmit(event) {
   event.preventDefault();
   var status = document.getElementById("status");
   var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
+
+  fetch("https://api.apispreadsheets.com/data/14316/", {
+    method: "POST",
     body: data,
-    headers: {
-      Accept: "application/json",
-    },
-  })
-    .then((response) => {
+  }).then((res) => {
+    if (res.status === 201) {
+      // SUCCESS
       status.innerHTML = "Thanks for your submission! We'll get back to you.";
       form.reset();
-    })
-    .catch((error) => {
+    } else {
+      // ERROR
       status.innerHTML = "Oops! server error, Please Try again later.";
-    });
+    }
+  });
 }
 form.addEventListener("submit", handleSubmit);
